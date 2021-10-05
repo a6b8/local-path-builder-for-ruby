cac1e5ff87eb8204cb1e04b0cb449e0bf1909d0e
a6b8 <andreasbanholzer@me.com>
2021-10-05 03:12:47 +0200

    v0.01

--- lib/local_path_builder.rb
+++ lib/local_path_builder.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "local_path_builder/version"
-require 'FileUtils'
+require 'fileutils'
 
 
 module LocalPathBuilder
--- test/circle_ci.rb
+++ test/circle_ci.rb
@@ -1,7 +1,4 @@
 require './lib/local_path_builder'
-require 'fileutils'  
-
-Gem::Specification.sort{|a,b| a.name <=> b.name}.map {|a| puts "#{a.name} (#{a.version})"; puts "-" * 50; puts a.homepage; puts a.description; puts "\n\n"}
 
 struct = LocalPathBuilder.helper()
 
